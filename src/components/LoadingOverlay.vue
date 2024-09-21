289bb276c0704036811180373ed24a033416271e
Alexandru Kitsenco <64800754+alexandrukitsenco@users.noreply.github.com>
2024-09-21 20:28:07 +0200

    fix

--- src/components/LoadingOverlay.vue
+++ src/components/LoadingOverlay.vue
@@ -1,4 +1,3 @@
-<!-- Overlay.vue -->
 <template>
   <div v-if="loadingStore.getLoadingState" class="fixed top-0 left-0 h-screen w-screen bg-black/50">
     <div class="flex items-center justify-center h-full w-full">
@@ -7,26 +6,9 @@
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import { useLoadingStore } from '../stores/loadingStore'
 import ProgressSpinner from 'primevue/progressspinner'
 
 const loadingStore = useLoadingStore()
 </script>
-
-<style scoped>
-/* Puedes agregar estilos adicionales aquí si es necesario */
-:deep(.p-overlaypanel) {
-  background-color: rgba(0, 0, 0, 0.5);
-}
-
-:deep(.p-progress-spinner) {
-  width: 50px;
-  height: 50px;
-}
-
-:deep(.p-progress-spinner-circle) {
-  stroke: #ffffff;
-  stroke-width: 3;
-}
-</style>
